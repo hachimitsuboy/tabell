d49b9d14426bd2a8ee79074f5f5312ea1b39977f
hachimitsuboy <39763423+hachimitsuboy@users.noreply.github.com>
2022-07-14 21:57:50 +0900

    サイトの構築

--- src/main.rs
+++ src/main.rs
@@ -32,83 +32,34 @@ struct AppArg {
 
 
 
-use reqwest::Client;
-type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 #[tokio::main]
-
-async fn main() -> Result<()>{
+async fn main() {
         // let _arg: AppArg = AppArg::parse();
         let args: Vec<String> = env::args().collect();
         let input = &args[1];
-    
-        let url = "https://api.codic.jp/v1/engine/translate.json?text=".to_string() + input + "&casing=camel";
-
-
-        let client = Client::new();
-        let res = client.get(url).header("Authorization", "Bearer n62BwX8uDpShpvXkBlnJNNsqrApyd9r0QI").header("Host", "api.codic.jp").send().await?;
-
-        let body = res.text().await?;
-
-    
-        let split_body: Vec<&str> = body.split("\"").collect();
-        println!("{:?}", split_body[9]);
-
-        
-
-        let json: serde_json::Value = serde_json::from_str(&body)?;
-
-        let obj = json.as_object().unwrap();
-
-        println!("{:?}", obj);
-
-
-        for (key,value) in obj.iter() {
-            println!("{:?}\t{:?}",key,value);
-            }
-    
-        eprintln!("*** 終了 ***");
-        Ok(())
-    
-
-            
-    
-        // println!("{}", type_of(&res));
-        // println!("Response: {:?}", res);
-
-        // let mut tagger = Tagger::new("");
-
-        // println!("{}", input);
-        // let mut result = tagger.parse_str(input);
-        // // println!("RESULT: {:?}", result);
-        // //改行で区切る
-        // let lineResult: Vec<&str> = result.as_str().split('\n').collect();
-         
-       
-        // //タブ文字で区切る。先頭を取り出す
-        
-        // for i in lineResult {
-        //     println!("{:?}", i)
-
-        // }
-        // // println!("{}", _arg.message);
-        
-
-
-
+        println!("input: {}", input);
+        let res = request(input).await;
+        println!("{}",type_of(&res));
 
 }
 
+use reqwest::Client;
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
-fn request(input: &str){
-    let url = "https://api.codic.jp/v1/engine/translate.json?text=".to_string() + input + "&casing=lower+underscore";
+async fn request(input: &str) -> Result<()>{
+ 
+    let url = "https://api.codic.jp/v1/engine/translate.json?text=".to_string() + input + "&casing=camel";
 
+    let client = Client::new();
+    let res = client.get(url).header("Authorization", "Bearer n62BwX8uDpShpvXkBlnJNNsqrApyd9r0QI").header("Host", "api.codic.jp").send().await?;
 
-    let client = reqwest::blocking::Client::new();
-    let res = client.get(url).header("Authorization", "Bearer n62BwX8uDpShpvXkBlnJNNsqrApyd9r0QI").header("Host", "api.codic.jp").send().unwrap().text();
+    let body = res.text().await?;
+    
+    let split_body: Vec<&str> = body.split("\"").collect();
+    println!("{:?}", split_body[9]);   
+    
 
-    println!("{}", type_of(&res));
-    println!("////////////////");
-    println!("Response: {:?}", res);
+    Ok(())
 
 }
 
@@ -124,6 +75,7 @@ fn hello(name: Option<String>) -> String {
     })
 }
 
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -133,5 +85,10 @@ mod tests {
         assert_eq!("Hello, World", hello(None));
         assert_eq!("Hello, Nagae", hello(Some("Nagae".to_string())));
     }
+
+    fn test_input() {
+        assert_eq!(Result<"largeClock",alloc::boxed::Box<dyn std::error::Error>>, request("大きな時計"));
+    }
+
 }
 
